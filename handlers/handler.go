ffc3a6558b1e72fe1b3f34c19c161d86a074963e
mRileyMacK <25649648+mRileyMacK@users.noreply.github.com>
2020-02-27 22:43:16 -0500

    Update handlers/handler.go
    Co-Authored-By: Thao-Tran Le-Phuong <Thao-Tran@users.noreply.github.com>

--- handlers/handler.go
+++ handlers/handler.go
@@ -58,7 +58,6 @@ func (env *Env) PostAuthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(reqUser)
-
 }
 
 func (env *Env) PostUserHandler(w http.ResponseWriter, r *http.Request) {
